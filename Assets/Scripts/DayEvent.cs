9c8fdbbd6d2f943e07bf5b01dcf7449b215afd18
alka <dylan.tanguy@gmail.com>
2015-11-21 18:58:01 +0900

    Memory.cs complet GameManager.cs avec TODO

--- Assets/Scripts/DayEvent.cs
+++ Assets/Scripts/DayEvent.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DayEvent : MonoBehaviour {
+public class DayEvent : MonoBehaviour
+{
+    public static DayEvent instance;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        GameManager.Instance.AddToDayEventList(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,25 +4,50 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    protected Hashtable statistics;     // Dictionnaire de stats
-    [SerializeField]
-    protected List<string> statNames;   // Liste des stats
+    protected List<DayEvent> dayEventList;  // Liste des dayEvent à jouer jusqu'à l'appel de la scène Night suivante
+    protected DayEvent dayEvent;
+    protected Hashtable statistics;         // Stats récupérées dans Memory
+    protected List<string> statNames;       // Nom des stats récupérés dans Memory
+    public static GameManager instance;
     private int i;
-    protected List<DayEvent> dayEvent;  // Liste des dayEvent à jouer jusqu'à l'appel de la scène Night suivante
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
 
     // Use this for initialization
     void Start()
     {
-        statistics = new Hashtable();
+        statistics = Memory.Instance.GetStatistics();
+        statNames = Memory.Instance.GetStatNames();
         for (i = 0; i < statNames.Count; i++)
         {
-            statistics[statNames[i]] = 0;
+            // TODO : REMPLIR EN DUR LES dayEvent A JOUER
+        }
+        OnDayEventFinished(); // Premier dayEvent du jour
+    }
+    
+    void OnDayEventFinished()
+    {
+        if (dayEventList.Count == 0)
+        {
+            // TODO : INDEXER LA SCENE
+            //Application.LoadLevel(2);
+        }
+        else
+        {
+            dayEvent = dayEventList[dayEventList.Count - 1];
+            dayEventList.Remove(dayEvent);
+            // TODO : LAUNCH dayEvent
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddToDayEventList(DayEvent dayEventToAdd)
     {
-
+        dayEventList.Add(dayEventToAdd);
     }
 }
